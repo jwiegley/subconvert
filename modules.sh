72e4110b70f1cc8b874e08d36111416b185a01f1
John Wiegley <johnw@newartisans.com>
2011-01-19 17:59:14 -0500

    Added modules.sh

---
+++ modules.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+
+TOP=/Volumes/RAID/Mirrors/boost
+
+BOOST_RYPPL=$TOP/ryppl
+BOOST_SVN=$TOP/boost-svn
+BOOST_GIT=$TOP/boost-git
+
+if [[ $1 == "reset" ]]; then
+    (cd $BOOST_GIT && \
+     git checkout master && \
+     git reset --hard origin/master && \
+     git submodule foreach git reset --hard origin/master && \
+     git clean -fdx && \
+     git status --porcelain | awk '{print $2}' | xargs rm -fr)
+else
+    (cd $BOOST_RYPPL/boost && \
+     python modularize.py -a --src=$BOOST_SVN --dst=$BOOST_GIT "$@")
+fi    
