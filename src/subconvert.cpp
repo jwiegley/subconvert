d5271ede268fdcd13e5ac2550eb2b3e8a2265b47
John Wiegley <johnw@newartisans.com>
2011-02-03 16:41:44 -0500

    Don't track files that only have property changes

--- src/subconvert.cpp
+++ src/subconvert.cpp
@@ -777,11 +777,12 @@ struct ConvertRepository
       assert(obj);
       assert(obj->is_blob());
       obj = obj->copy_to_name(pathname.filename().string());
-    } else {
+    }
+    else if (! (node.get_action() == SvnDump::File::Node::ACTION_CHANGE &&
+                ! node.has_text())) {
       obj = repository.create_blob(pathname.filename().string(),
-                                   node.has_text() ?
-                                   node.get_text() : "",
-                                   node.get_text_length());
+                                   node.has_text() ? node.get_text() : "",
+                                   node.has_text() ? node.get_text_length() : 0);
     }
     rev_tree->update(pathname, obj);
     return true;
