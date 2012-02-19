aea2d462d9add5e94028ff2fcaf9cbbe8e8f1ae3
John Wiegley <johnw@newartisans.com>
2012-02-19 00:46:02 -0600

    Added comment to py/compare-revs.py

--- py/compare-revs.py
+++ py/compare-revs.py
@@ -38,4 +38,16 @@ def checkout_and_compare(svn_url, svn_rev, git_dir, git_sha, tmp_dir):
         os.delete(git_tmp)
 
 if __name__ == "__main__":
+    # What needs to happen here is:
+    #
+    # 1. Create a list of Git commits in the flat history.
+    # 2. Choosing a series of them, read the commit message to find the
+    #    corresponding Subversion revision.
+    # 3. Call checkout_and_compare to checkout both working trees in both
+    #    systems, and then compare them byte-wise for identity.
+    # 4. This same algorithm should work for comparing branches with branch
+    #    contents in Subversion, but I haven't represented that logic above
+    #    yet.
+    # 5. This can all likely be done in a much cleaner, more Python-friendly
+    #    way, but I was seeking the quickest path to a trustable answer.
     checkout_and_compare(*sys.argv[1:])
