4897f5142c3093b36aaaad5368d84921127bf7d5
John Wiegley <johnw@newartisans.com>
2012-02-21 15:38:23 -0600

    More refinements to update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -1,60 +1,80 @@
 #!/bin/bash
 
-LOG=$HOME/Library/Logs/boost-update.log
-
-#RAMDISK=/tmp/ramdisk
-RAMDISK=$HOME/Products
-
 set -e
 
+error() {
+    cat <<EOF | mutt -a ~/Library/Logs/boost-update.log \
+        -s '[boost] Migration update FAILED' johnw@boostpro.com
+Boost migration update build FAILED.
+EOF
+}
+
+trap 'error ${LINENO}' ERR
+
+exec > ~/Library/Logs/boost-update.log 2>&1
+
+RAMDISK=/tmp/ramdisk
 BOOST=/Volumes/Data/Mirrors/Boost
 MIGRATE=$HOME/Contracts/BoostPro/Projects/boost-migrate
 
-$MIGRATE/bin/modules.sh reset > $LOG 2>&1
+$MIGRATE/bin/modules.sh reset
 
 cd $BOOST
 
-(cd boost-git; git pull; git submodule update --init) >> $LOG 2>&1
-(cd boost-private; git pull) >> $LOG 2>&1
-(cd boost-svn; git pull) >> $LOG 2>&1
-(cd Boost.Defrag; git pull) >> $LOG 2>&1
-(cd installer; git pull) >> $LOG 2>&1
-(cd ryppl; git pull) >> $LOG 2>&1
-(cd boost-modularize; git pull) >> $LOG 2>&1
+(cd boost-git; git pull; git submodule update --init)
+(cd boost-private; git pull)
+(cd boost-svn; git pull)
+(cd Boost.Defrag; git pull)
+(cd installer; git pull)
+(cd ryppl; git pull)
+(cd boost-modularize; git pull)
 
-svnsync --non-interactive sync file://$PWD/boost.svnrepo >> $LOG 2>&1
+svnsync --non-interactive sync file://$PWD/boost.svnrepo
+svnadmin dump -q boost.svnrepo > boost.svnrepo.dump
+
 perl -i -pe "s%url =.*%url = file://$PWD/boost.svnrepo%;" boost-clone/.git/config
-(cd boost-clone; git svn fetch; git reset --hard trunk) >> $LOG 2>&1
+(cd boost-clone; git svn fetch; git reset --hard trunk)
 
-$MIGRATE/bin/modules.sh update >> $LOG 2>&1
-#$MIGRATE/bin/modules.sh push >> $LOG 2>&1
+if [[ ! -d $RAMDISK ]]; then
+    mkramdisk
 
-time svnadmin dump -q boost.svnrepo | xz -9ec > boost.svnrepo.dump.xz 2>> $LOG
-
-#if [[ ! -d $RAMDISK/cpp ]]; then
-#    if [[ ! -d $RAMDISK ]]; then
-#        mkramdisk
-#        mkramdisk
-#    fi
-#    mkdir $RAMDISK/cpp
-#fi
+    if [[ -d $RAMDISK ]]; then
+        mkdir $RAMDISK/cpp
+    fi
+fi
 
 if [[ -d $RAMDISK/cpp ]]; then
     /bin/rm -fr $RAMDISK/cpp
     mkdir $RAMDISK/cpp
     cd $RAMDISK/cpp
 
-    git init >> $LOG 2>&1
-    $MIGRATE/subconvert -A $MIGRATE/doc/authors.txt -B $MIGRATE/doc/branches.txt \
-        convert $BOOST//boost.svnrepo.dump >> $LOG 2>&1
-    git symbolic-ref HEAD refs/heads/trunk >> $LOG 2>&1
-    git checkout trunk >> $LOG 2>&1
-    git gc >> $LOG 2>&1
-    git remote add origin git@github.com:boost-lib/boost-history.git >> $LOG 2>&1
+    git init
+    time $MIGRATE/subconvert                                            \
+        -A $MIGRATE/doc/authors.txt                                     \
+        -B $MIGRATE/doc/branches.txt                                    \
+        convert $BOOST//boost.svnrepo.dump
+    git symbolic-ref HEAD refs/heads/trunk
+    git checkout trunk
+    git gc
+
     sleep 300
-    git push -f --all origin >> $LOG 2>&1
-    git push -f --mirror origin >> $LOG 2>&1
-    git push -f --tags origin >> $LOG 2>&1
+
+    #git remote add origin git@github.com:boost-lib/boost-history.git
+    #git push -f --all origin
+    #git push -f --mirror origin
+    #git push -f --tags origin
+
+    rsync -av --delete .git/ $BOOST/boost-history.git/
+    cd $BOOST
+    diskutil eject $RAMDISK
 fi
 
-mv $LOG $BOOST
+$MIGRATE/bin/modules.sh update
+#$MIGRATE/bin/modules.sh push
+
+cat <<EOF | mutt -a ~/Library/Logs/boost-update.log \
+    -s '[boost] Migration update succeeded' johnw@boostpro.com
+Boost migration update build succeeded.
+EOF
+
+exit 0
