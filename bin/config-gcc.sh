1dc5c8105fe20a03e1f9320921d1ab6357765b4e
John Wiegley <johnw@newartisans.com>
2012-04-21 04:08:09 -0500

    Updated config scripts

---
+++ bin/config-gcc.sh
@@ -0,0 +1,1 @@
+./configure --prefix=/usr/local/stow/subconvert CC=gcc-mp-4.8 LD=g++-mp-4.8 CXX=g++-mp-4.8 CPPFLAGS="-isystem /opt/local/include -isystem /usr/local/include" LDFLAGS="-L/opt/local/lib -L/usr/local/lib"
